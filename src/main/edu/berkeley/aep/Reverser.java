60f6fec1ecaf14914b7c963fc232d08f2543b126
Jez Humble <jez@jezhumble.net>
2015-03-06 15:57:18 -0800

    Made the tests break

--- src/main/edu/berkeley/aep/Reverser.java
+++ src/main/edu/berkeley/aep/Reverser.java
@@ -9,8 +9,6 @@ public class Reverser {
     }
 
     public String reverse() {
-        if (toReverse == null || toReverse.length() < 2)
-            return toReverse;
         return new Reverser(toReverse.substring(1)).reverse() + toReverse.charAt(0);
     }
 }
